f68e4d65537a2db787bbd195b1c221e1f63b4ea7
cangli <174418702+s0raLin@users.noreply.github.com>
2026-02-20 20:57:52 +0800

    Code/模块

---
+++ Code/module/index.ts
@@ -0,0 +1,5 @@
+import me,{cat} from './me.ts';
+
+console.log(me);
+
+console.log(cat);
---
+++ Code/module/me.ts
@@ -0,0 +1,11 @@
+export default [
+    'cat', 'dog', 'bird'
+];
+
+
+export const cat = {
+    name: 'Bob',
+    voice: 'meow',
+    colors: ['black', 'orange'],
+};
+
